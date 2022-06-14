3c46638bb6c2c94e39e9357f46b809415920ec58
Filip Slavkovic <slavkovicf@bzz.ch>
2022-06-14 21:45:51 +0200

    kommentiert

--- src/main/java/ch/bzz/videothek/model/Film.java
+++ src/main/java/ch/bzz/videothek/model/Film.java
@@ -20,7 +20,6 @@ import java.time.LocalDate;
  */
 
 public class Film {
-    //@Pattern(regexp="[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}")
 
     @FormParam("filmUUID")
     @Pattern(regexp = "[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}")
--- src/main/java/ch/bzz/videothek/util/LocalDateSerializer.java
+++ src/main/java/ch/bzz/videothek/util/LocalDateSerializer.java
@@ -8,12 +8,25 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * custom Serializer for jackson-databind
+ */
 public class LocalDateSerializer extends StdSerializer<LocalDate> {
 
+    /**
+     * constructor
+     */
     public LocalDateSerializer() {
         super(LocalDate.class);
     }
 
+    /**
+     * converts a data from LocalDate to String
+     * @param value
+     * @param generator
+     * @param provider
+     * @throws IOException
+     */
     @Override
     public void serialize(LocalDate value, JsonGenerator generator, SerializerProvider provider) throws IOException {
         generator.writeString(value.format(DateTimeFormatter.ISO_DATE));
